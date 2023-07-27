bf1ffd3864660d86c1c30eb6adb345a69b0839d2
maiguel <itsenergyy05@gmail.com>
2023-07-27 14:40:24 -0400

    add method that returns todays forecast

--- page-objects/hourly-forecast-page.ts
+++ page-objects/hourly-forecast-page.ts
@@ -1,5 +1,13 @@
 import { expect, type Locator, type Page } from '@playwright/test';
 
+type WeatherInfo = {
+  time: string;
+  temperature: string;
+  thermalSensation: string;
+  chanceOfPrecipitation: string;
+  cloudCover: string;
+};
+
 export class HourlyForecastPage {
   readonly page: Page;
   readonly cityHeader: Locator;
@@ -12,6 +20,49 @@ export class HourlyForecastPage {
   async goto(city: string) {
     await this.page.goto(
       `https://www.wunderground.com/hourly/do/santo-domingo/${city}`
+    );
+  }
+
+  async getHourlyData(): Promise<WeatherInfo[]> {
+    return this.page.$$eval(
+      '#hourly-forecast-table > tbody > tr',
+      (all_rows) => {
+        const hourlyWeatherInfo: WeatherInfo[] = [];
+
+        all_rows.forEach((hour_row) => {
+          //TODO: make selectors better and try POM.
+          const time =
+            hour_row.querySelector('td[class*="timeHour"] > span')
+              ?.textContent ?? 'ERROR, check code.';
+          const temperature =
+            hour_row.querySelector(
+              'td[class*="temperature"] > lib-display-unit > span'
+            )?.textContent ?? 'ERROR, check code.';
+          const thermalSensation =
+            hour_row.querySelector(
+              'td[class*="feelsLike"] > lib-display-unit > span'
+            )?.textContent ?? 'ERROR, check code.';
+          const chanceOfPrecipitation =
+            hour_row.querySelector(
+              'td[class*="precipitation"] > a > lib-display-unit > span'
+            )?.textContent ?? 'ERROR, check code.';
+          const cloudCover =
+            hour_row.querySelector(
+              'td[class*="cloudCover"] > lib-display-unit > span'
+            )?.textContent ?? 'ERROR, check code.';
+          // console.log(
+          //   `Time: ${time}, Temperature: ${temperature}, Thermal Sensation - Feels Like: ${thermalSensation}, Chance of Precipitation: ${chanceOfPrecipitation}, Cloud Cover: ${cloudCover}`
+          // );
+          hourlyWeatherInfo.push({
+            time: time,
+            temperature: temperature,
+            thermalSensation: thermalSensation,
+            chanceOfPrecipitation: chanceOfPrecipitation,
+            cloudCover: cloudCover,
+          });
+        });
+        return hourlyWeatherInfo;
+      }
     );
   }
 }
--- tests/santo-domingo.spec.ts
+++ tests/santo-domingo.spec.ts
@@ -5,30 +5,8 @@ test('weather', async ({ page }) => {
   const HourlyForecast = new HourlyForecastPage(page);
   const city: string = 'ISANTO172';
   await HourlyForecast.goto(city);
-  const rows = await page.$$eval(
-    '#hourly-forecast-table > tbody > tr',
-    (all_rows) => {
-      all_rows.forEach((hour_row) => {
-        //TODO: make selectors better and try POM.
-        const time = hour_row.querySelector(
-          'td[class*="timeHour"] > span'
-        )?.textContent;
-        const temperature = hour_row.querySelector(
-          'td[class*="temperature"] > lib-display-unit > span'
-        )?.textContent;
-        const thermalSensation = hour_row.querySelector(
-          'td[class*="feelsLike"] > lib-display-unit > span'
-        )?.textContent;
-        const chanceOfPrecipitation = hour_row.querySelector(
-          'td[class*="precipitation"] > a > lib-display-unit > span'
-        )?.textContent;
-        const cloudCover = hour_row.querySelector(
-          'td[class*="cloudCover"] > lib-display-unit > span'
-        )?.textContent;
-        console.log(
-          `Time: ${time}, Temperature: ${temperature}, Thermal Sensation - Feels Like: ${thermalSensation}, Chance of Precipitation: ${chanceOfPrecipitation}, Cloud Cover: ${cloudCover}`
-        );
-      });
-    }
-  );
+  const dayForecast = await HourlyForecast.getHourlyData(); //good const.
+  // dayForecast.forEach((x) => {
+  //   console.log(`lol${x.time}, ${x.chanceOfPrecipitation}`);
+  // });
 });
