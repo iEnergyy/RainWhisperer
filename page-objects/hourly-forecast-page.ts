baecbd2cfc2e70adf704b28554074eedea653118
iEnergyy <44242768+iEnergyy@users.noreply.github.com>
2023-07-27 16:44:57 -0400

    First scrape attempt (#1)
    * delete firefox and webkit instances
    
    * getting todays info working
    
    * add method that returns todays forecast
    
    * csv is being generated
    
    * clean the comments

--- page-objects/hourly-forecast-page.ts
+++ page-objects/hourly-forecast-page.ts
@@ -1,4 +1,13 @@
 import { expect, type Locator, type Page } from '@playwright/test';
+import * as Papa from 'papaparse';
+
+type WeatherInfo = {
+  time: string;
+  temperature: string;
+  thermalSensation: string;
+  chanceOfPrecipitation: string;
+  cloudCover: string;
+};
 
 export class HourlyForecastPage {
   readonly page: Page;
@@ -13,5 +22,54 @@ export class HourlyForecastPage {
     await this.page.goto(
       `https://www.wunderground.com/hourly/do/santo-domingo/${city}`
     );
+  }
+
+  async getHourlyData(): Promise<WeatherInfo[]> {
+    return this.page.$$eval(
+      '#hourly-forecast-table > tbody > tr',
+      (all_rows) => {
+        const hourlyWeatherInfo: WeatherInfo[] = [];
+
+        all_rows.forEach((hour_row) => {
+          //TODO: make selectors better and try POM.
+          const time =
+            hour_row.querySelector('td[class*="timeHour"] > span')
+              ?.textContent ?? 'ERROR, check code.';
+          const temperature =
+            hour_row.querySelector(
+              'td[class*="temperature"] > lib-display-unit > span'
+            )?.textContent ?? 'ERROR, check code.';
+          const thermalSensation =
+            hour_row.querySelector(
+              'td[class*="feelsLike"] > lib-display-unit > span'
+            )?.textContent ?? 'ERROR, check code.';
+          const chanceOfPrecipitation =
+            hour_row.querySelector(
+              'td[class*="precipitation"] > a > lib-display-unit > span'
+            )?.textContent ?? 'ERROR, check code.';
+          const cloudCover =
+            hour_row.querySelector(
+              'td[class*="cloudCover"] > lib-display-unit > span'
+            )?.textContent ?? 'ERROR, check code.';
+          hourlyWeatherInfo.push({
+            time: time,
+            temperature: temperature,
+            thermalSensation: thermalSensation,
+            chanceOfPrecipitation: chanceOfPrecipitation,
+            cloudCover: cloudCover,
+          });
+        });
+        return hourlyWeatherInfo;
+      }
+    );
+  }
+
+  convertToCSV(data: WeatherInfo[]): string {
+    const csv = Papa.unparse(data, {
+      header: true,
+      quotes: true,
+    });
+
+    return csv;
   }
 }
--- tests/santo-domingo.spec.ts
+++ tests/santo-domingo.spec.ts
@@ -1,8 +1,13 @@
 import { test, expect } from '@playwright/test';
 import { HourlyForecastPage } from '../page-objects/hourly-forecast-page';
+import fs from 'fs';
+import * as Papa from 'papaparse';
 
 test('weather', async ({ page }) => {
   const HourlyForecast = new HourlyForecastPage(page);
   const city: string = 'ISANTO172';
   await HourlyForecast.goto(city);
+  const dayForecast = await HourlyForecast.getHourlyData();
+  const csvData = HourlyForecast.convertToCSV(dayForecast);
+  fs.writeFileSync('weather_forecast.csv', csvData);
 });
